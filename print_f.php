820a4bdda5fa749b0184f49d79519b762564d54a
Leonel94aguirre <aguirreleonel194@gmail.com>
2022-03-17 20:36:54 -0300

    cambios

--- print_f.php
+++ print_f.php
@@ -1,17 +1,5 @@
 <?php
-//Desarrollo
-function print_f($variable){
-    if (is_array($variable)) {
-        //Si es un array lo recorro y guardo el contenido en el archivo "datos.txt" ....
-        foreach ($variable as $item){
 
-        }
-    }
-    else {
-        //Entonces es string, guardo el contenido en el archivo "datos.txt"
-        file_put_contents("datos.txt", $variable);
-    }
-}
 function print_f1($variable){
     $archivo = fopen("datos.txt", "a+");
 
@@ -26,7 +14,7 @@ function print_f1($variable){
     fclose("datos.txt");
 }
 //Uso
-//$aNotas = array(8,5,7,9,10);
+//$aNotas = array(8,5,7,9,10)
 $msg = "Este es un mensaje";
 print_f1($msg);
 
